0ed4e4d25ec37eb2d4513510d22e91917d353a64
SrbastianM <sebas907.l@gmail.com>
2025-04-02 19:33:37 -0500

    feat: create a paragraph component to use in the show more content movie

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css'
 import SearchBar from './components/molecules/search-bar/SearchBar'
-import Card from './components/molecules/card/Card'
 
 function App() {
-  const text = "2014"
   return (
     <>
-      <SearchBar />
-      <Card title='Interstellar' description={text} imageSrc=''/>
+      <SearchBar /> 
     </>
   )
 }
